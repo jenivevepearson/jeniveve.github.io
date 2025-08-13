92bb29d77cd94ba021cce6f39972b76b16a562d3
Jeniveve Pearson <jeniveve.pearson@gmail.com>
2025-08-13 12:18:35 -0700

    adding debug function to make sure vercel is working

---
+++ api/debug.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  try {
+    const axiosPath = require.resolve("axios");
+    res.status(200).json({ axiosPath });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
--- api/nasa-ads.js
+++ api/nasa-ads.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
-const axios = require("axios").default;
+import axios from "axios";
 
 export default async function handler(req, res) {
   try {
